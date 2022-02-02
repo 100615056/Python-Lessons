07e815e1883d4eadd87117ecae6c5cc7264b8a8d
Angelica <32560782+100615056@users.noreply.github.com>
2022-02-02 00:39:51 -0500

    Print using loops

--- Pyramid/print.py
+++ Pyramid/print.py
@@ -12,20 +12,27 @@ for item in groceries:
 #2nd way - Variable
 #range (start, stop, step)
 for i in range(10):
-print(i)
+  print(i)
 
-# Challenge
-#Print two rows of ? with 4 ? each
-# 1st While Loop
+# While Statement
+i = 0 
+num = 6
+while i < num:
+  print(i, end= '\t')
+  i += 1 # i = i + 1
+print()
+
+# Challenge: Print two rows of ? with 4 ? each
+# 1st - While Loop
 #i = 0
 while i < 2:
   print("????")
   i += 1
-# 2nd for loop
+# 2nd - for loop
 for j in range(2):
   print('????')
   
-# 3rd for loop
+# 3rd - for loop
 for i in range(2):
   for k in range(4):
     print("?", end = "")
