fef785d0f8599ff000522ae814fe39ea4a6c5ecd
Angelica <32560782+100615056@users.noreply.github.com>
2022-02-02 00:43:15 -0500

    Create left-aligned pyramid

---
+++ Pyramid/left-aligned.py
@@ -0,0 +1,24 @@
+# Print left-aligned pyramid given valid user input
+
+# Validate user-input
+def pyramid_height():
+  while True:
+    # Get Number
+    number = int(input("Enter Number: "))
+    # Check that number is within range
+    if number >= 1 and number <= 8:
+      break
+  return number
+
+# Print left-aligned pyramid
+def print_pyramid(number):
+  for i in range(number):
+    for j in range(i + 1):
+      print('#', end='')
+    print()
+
+def main():
+  number = positive_number()
+  print_pyramid(number)
+
+main()
