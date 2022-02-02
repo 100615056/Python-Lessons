3e053fb80ae12e2f2d79c771309793b1857ed00f
Angelica <32560782+100615056@users.noreply.github.com>
2022-02-02 00:35:51 -0500

    Positive number

---
+++ Pyramid/positive.py
@@ -0,0 +1,24 @@
+# Make sure the user enters a positive number
+
+# Do-While Loop (Induce a infinite loop)
+# In order to exit the loop, break statement
+# Return value
+
+def main():
+  num = postive_number()
+  print(num)
+  
+def postive_number():
+  while True:
+  # Get Number
+    n = int(input("Enter positive number: "))
+  # If positive number, exit 
+  # Else ask again
+    if number > 0:
+      break
+  return number
+
+
+main()
+
+
