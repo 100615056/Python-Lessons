f26ed3fd41e3675cbb2bf23759b6e884d38b8de1
Angelica <32560782+100615056@users.noreply.github.com>
2022-03-07 18:59:55 -0500

    Update comments and print statements

--- Phonebook/phonebook.py
+++ Phonebook/phonebook.py
@@ -20,7 +20,7 @@ def contact_information():
     print('Creating Contact - Please Enter information')
     while True:
         check = input('Name: ')
-        # Return the name as a lowercase
+        # Change the name into lowercase
         name = check.lower()
         number = input('Enter Number: ')
         if name and number:
@@ -29,7 +29,7 @@ def contact_information():
 
 
 def create_contact(name, number):
-    # Create contact in dictionary
+    # Create contact in phone book
     PHONEBOOK[name] = number
 
 
@@ -37,7 +37,9 @@ def display_phonebook():
     # Display contact entries
     if not any(PHONEBOOK):
         print('No Contacts')
-
+        
+    # Print phone book contacts
+    print('Displaying Contacts')
     for name, number in sorted(PHONEBOOK.items()):
         print()
         print(f'Name: {name.title()}')
@@ -47,11 +49,11 @@ def display_phonebook():
 
 def search_contact():
     # Search for contacts in dictionary
-    print('Searching Contacts...')
     # Look up contact by name
     check = input('Enter contact name: ')
     # Convert contact name to lowercase
     name = check.lower()
+    print('Searching Contacts...')
     if name in PHONEBOOK:
         print(f'Number: {PHONEBOOK[name]}')
     else:
@@ -60,27 +62,25 @@ def search_contact():
 
 def update_contact():
     # Update contact information
-    print('Update Contact...')
     check = input('Enter contact name: ')
     name = check.lower()
     if name in PHONEBOOK:
         number = input('Enter new number: ')
+        print('Updating Contact...')
         PHONEBOOK[name] = number
         print('Contact Updated')
-
     else:
         print('Contact not Found')
 
 
 def delete_contact():
     # Remove contact entry
-    print('Delete Contact...')
     check = input('Enter contact name: ')
     name = check.lower()
     if name in PHONEBOOK:
+        print('Deleting Contact...')
         PHONEBOOK.pop(name)
         print('Contact Removed')
-
     else:
         print('Contact not Found')
 
@@ -96,17 +96,23 @@ def main():
         else:
             continue
         # Menu choices
+        # Create Contact
         if choice == 1:
             name, number = contact_information()
             create_contact(name, number)
+        # Disply Contacts
         if choice == 2:
             display_phonebook()
+        # Search for a Contact
         if choice == 3:
             search_contact()
+        # Update a Contact
         if choice == 4:
             update_contact()
+        # Delete a contact
         if choice == 5:
             delete_contact()
+        # Exit
         if choice == 6:
             break
 
