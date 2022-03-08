7d2c4c6c81fb6f6bdde779564d56e4885e773386
Angelica <32560782+100615056@users.noreply.github.com>
2022-03-07 19:06:11 -0500

    Lesson on deleting dictionary items using pop()

---
+++ Phonebook/delete.py
@@ -0,0 +1,8 @@
+"""
+The pop() method removes and returns an element from a dictionary having the given key.
+"""
+books = {"Harry Potter": 10, "Hunger Games": 8, "Percy Jackson": 7}
+
+print('Original:', books)
+books.pop('Hunger Games')
+print('Edited:', books)
